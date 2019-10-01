1687fcf1f3b5eade79747fa5587471e737a28270
MaryDuk <54117390+MaryDuk@users.noreply.github.com>
2019-10-01 22:18:44 +0300

    TestData1 class added

---
+++ first-project/src/test/java/TestData1.java
@@ -0,0 +1,41 @@
+public class TestData1 {
+    public String num1;
+    public String num2;
+    public String num3;
+    public String num4;
+    public String num5;
+    public String num6;
+    public String num7;
+
+    public String getNum1(){ return num1; }
+    public String getNum2(){ return num2; }
+    public String getNum3(){ return num3; }
+    public String getNum4(){ return num4; }
+    public String getNum5(){ return num5; }
+    public String getNum6(){ return num6; }
+    public String getNum7(){ return num7; }
+
+    public TestData1 setNum1(String num1){
+        this.num1 = num1;
+        return this; }
+    public TestData1 setNum2(String num2){
+        this.num2 = num2;
+        return this; }
+    public TestData1 setNum3(String num3){
+        this.num3 = num3;
+        return this; }
+    public TestData1 setNum4(String num4){
+        this.num4 = num4;
+        return this; }
+    public TestData1 setNum5(String num5){
+        this.num5 = num5;
+        return this; }
+    public TestData1 setNum6(String num6){
+        this.num6 = num5;
+        return this; }
+    public TestData1 setNum7(String num7){
+        this.num7 = num7;
+        return this; }
+
+}
+
